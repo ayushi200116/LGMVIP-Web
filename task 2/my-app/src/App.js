e7ad7c2ab667dc2933059d2f8c94e5edeba9e3ed
Ayushi Indu <singhayushi535@gmail.com>
2022-01-27 01:44:50 +0530

    modified the code

--- task 2/my-app/src/App.js
+++ task 2/my-app/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import {PacmanLoader} from 'react-spinners';
 function App() {
+    function delay(delayInms) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+        resolve(2);
+        }, delayInms);
+      });
+    }
+    const [users, setUser] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const loadUsers = async() => {
+      setLoading(true);
+      const response = await fetch("https://reqres.in/api/users?page=1");
+      const jsonResponse = await response.json();
+      await delay(3000);
+      setUser(jsonResponse.data);
+      setLoading(false);
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+        <h1>BAKERY SHOP</h1>
+        <button onClick={loadUsers}>GET USERS</button>
+      </nav>
+      {loading ? <PacmanLoader color='white' css={{position: "fixed", top: "50%", right: "50%"}} size={40}/> : <>
+        {users.map(({ id, first_name, last_name, avatar, email}) => (
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+            <img src={avatar} alt=''/>  
+            </div>
+            <div class="flip-card-back">
+              <br></br>
+              <h3>ID </h3> <p>{id}</p>
+              <h3>NAME</h3> <p>{first_name} {last_name}</p>
+              <h3> EMAIL</h3> <p>{email}</p>
+           </div>
+          </div>
+          </div>
+        ))}</>}
     </div>
-  );
-}
+  );}
 
 export default App;
